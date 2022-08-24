febfaba1284a026a5edd7686c7df46dfc772bbe2
Maikoll Soto <maikoll120@gmail.com>
2022-08-24 15:43:17 -0400

    Config Styled Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import GlobalStyle from '../styles/global'
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#111',
+    secondary: '#0070f3',
+  },
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
